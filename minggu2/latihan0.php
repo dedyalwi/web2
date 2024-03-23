a3ab7df80a158fac7c10fe0d7b27367652165b3b
DEDY ALWI <145128126+dedyalwi@users.noreply.github.com>
2024-03-23 10:34:36 +0700

    metode request

---
+++ minggu2/latihan0.php
@@ -0,0 +1,12 @@
+<?php
+
+//lingkup variabel
+
+$angka = 10;
+function Angka(){
+    $angka = 20;
+    echo $angka;
+}
+echo $angka;
+echo "<br>";
+echo Angka();
--- minggu2/latihan1.php
+++ minggu2/latihan1.php
@@ -1,12 +1,35 @@
 <?php
+$mahasiswa = [
+    ["nama" =>"Eka Yuniar",
+     "nim" => "10510016",
+     "jurusan" => "Sistem Informasi",
+     "email" => "eka@stimata.ac.id",
+     "gambar" => "eka.jpg"
+    ],
 
-//lingkup variabel
+    ["nama" => "Dedy Alwi",
+    "nim" => "23510015",
+    "jurusan" => "Sistem Informasi",
+    "email" => "dedy@stimata.ac.id",
+    "gambar" => "dedy.jpg"
+    ]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan GET</title>
+</head>
+<body>
+   <h1>Data Mahasiswa</h1>
+   <ul>
+   <?php foreach ($mahasiswa as $mhs) : ?>
+        <li>
+            <a href="latihan2.php?nama=<?= $mhs["nama"]; ?>&nim=<?= $mhs["nim"]; ?>&jurusan=<?= $mhs["jurusan"]; ?>&email=<?= $mhs["email"]; ?>&gambar=<?= $mhs["gambar"]; ?>"><?= $mhs["nama"]; ?></a>
+        </li>
 
-$angka = 10;
-function Angka(){
-    $angka = 20;
-    echo $angka;
-}
-echo $angka;
-echo "<br>";
-echo Angka();
+   <?php endforeach; ?>
+   </ul>
+</body>
